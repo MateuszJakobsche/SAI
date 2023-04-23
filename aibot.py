614fd32e4092c1e62bc0477d3729d099a81c2362
MateuszJakobsche <92634489+MateuszJakobsche@users.noreply.github.com>
2023-04-23 13:57:39 +0200

    SAI v.1.1
    Enhanced interface allowing live testing of configuration.

--- aibot.py
+++ aibot.py
@@ -1,40 +1,61 @@
 import openai
 import streamlit as st
-from streamlit_chat import message
-from config import APIkey, USERname, AIname, AIidentity
+from config import APIkey, USERname, SAIname, SAIidentity
 
 #functions
+def initialize_session():
+    if 'prompts' not in st.session_state:
+        st.session_state['prompts'] = [{"role": "system", "content": saiIdentity}]
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
+
+def restore_session():
+    del st.session_state['prompts']
+    del st.session_state['generated']
+    del st.session_state['past']
+    initialize_session()
+
 def generate_response(prompt):
     st.session_state['prompts'].append({"role": "user", "content":prompt})
     completion=openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages = st.session_state['prompts']
     )
-    message= completion.choices[0].message.content
+    message = completion.choices[0].message.content
     return message
+
 def send_click():
     if st.session_state['user']!= '':
-        chat_input = USERname + st.session_state['user']
+        chat_input = st.session_state['user']
         chat_output= generate_response(chat_input)
         st.session_state['past'].append(chat_input)
         st.session_state['generated'].append(chat_output)
         st.session_state['prompts'].append({"role": "assistant", "content": chat_output})
         st.session_state['user'] = ""
 
-#initialization
-openai.api_key = APIkey
-if 'prompts' not in st.session_state:
-    st.session_state['prompts'] = [{"role": "system", "content": AIidentity}]
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
+#configuration menu
+with st.sidebar:
+    openai.api_key = st.text_input("Your API key*", value=APIkey, type="password")
+    st.divider()
+    userName = st.text_input("Your name", value=USERname) + ": "
+    saiName = st.text_input("SAI's name", value=SAIname) + ": "
+    st.divider()
+    saiIdentity = st.text_area("SAI's identity", value=SAIidentity)
+    st.button("restore identity", on_click=restore_session)
 
-#content
+initialize_session()
+
+#chat content
 user_input=st.text_input("enter message", key="user", label_visibility="collapsed", on_change=send_click)
 with st.container():
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])-1, -1, -1):
-            st.divider()
-            st.markdown(st.session_state['past'][i])
-            st.markdown(AIname + st.session_state['generated'][i])
+            query = st.session_state['past'][i]
+            if len(query) > 50:
+                query = query[:50] + "(...)"
+
+            with st.expander(userName + query, expanded=1):
+                st.divider()
+                st.markdown(saiName + st.session_state['generated'][i])
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-APIkey = "yourAPIkey"
-USERname = "yourUSERname" + ": "
-AIname = "yourAIname" + ": "
-AIidentity = "how would you like them to behave, i.e.: You are helpful and polite"
+APIkey = ""
+USERname = "YOU"
+SAIname = "SAI"
+SAIidentity = ""
